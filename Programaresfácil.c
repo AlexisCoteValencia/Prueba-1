0bf75746d5471e54cdc00ac24c9ea376649bdb45
fernanda-11 <72525139+fernanda-11@users.noreply.github.com>
2020-10-07 16:42:29 -0500

    Agregue mi nombre amix :)

--- Programaresfácil.c
+++ Programaresfácil.c
@@ -3,5 +3,6 @@ inta main (){
   printf("Este es mi segundo nombre")
   printf("Programar es fácil")
   printf("Voy a compartir con mi compañero el código")
+    printf("\nCorona Hernández Fernanda\n")
   return 0;
 }
